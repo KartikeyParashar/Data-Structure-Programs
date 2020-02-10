93f11e59ee4270a18500da247a522959969a59bb
Kartikey Parashar <60282740+KartikeyParashar@users.noreply.github.com>
2020-02-10 16:26:58 +0530

    Update store_prime_in_2D_array.py

--- store_prime_in_2D_array.py
+++ store_prime_in_2D_array.py
@@ -4,7 +4,20 @@ from Data_Structure_Utils.PrimeNumbers import Prime
 def main():
     object_of_prime = Prime()
     object_of_prime.prime_in_range(1000)
-    return object_of_prime.prime_in_two_d_array()
+
+    lower_limit = 0
+    higher_limit = lower_limit + 100
+    arr = []
+    for i in range(1, 11):
+        arr1 = []
+        for num in object_of_prime.prime:
+            if lower_limit < num < higher_limit:
+                arr1.append(num)
+        arr.append(arr1)
+        lower_limit = higher_limit
+        higher_limit = lower_limit + 100
+
+    print(arr)
 
 
 main()
