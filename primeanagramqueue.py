b624cbc8c6944065e74c266a9b8a8b43f733393b
Kartikey Parashar <60282740+KartikeyParashar@users.noreply.github.com>
2020-02-10 16:29:15 +0530

    Update primeanagramqueue.py

--- primeanagramqueue.py
+++ primeanagramqueue.py
@@ -6,10 +6,13 @@ from Data_Structure_Utils.linked_list import Linked_List
 obj_of_ll = Linked_List()
 prime_in_range(1000)
 
-for num in prime:
-    obj_of_ll.insert_at_end(num)
+
+
+
+for number in prime:
+    if number[::-1] in prime:
+        obj_of_ll.insert_at_end(number)
 
 obj_of_ll.traverse()
-for number in obj_of_ll.elements:
-    if number[::-1] in obj_of_ll.elements:
-        print(number, end=' ')
+for num in obj_of_ll.elements:
+    print(num, end=' ')
